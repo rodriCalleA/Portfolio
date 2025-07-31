1c55e953dbfbcf3a55e78f4b79c7c584f80dea90
Rodrigo de la Calle <rodricalle@usal.es>
2025-07-31 15:32:27 +0200

    Actualiza la sección "Sobre mí" y el texto de presentación en el archivo index.html; modifica las cadenas de texto en typed.js para reflejar la experiencia y especialización actual.

--- src/js/typed.js
+++ src/js/typed.js
@@ -1,7 +1,7 @@
 import Typed from 'typed.js'
 
 var options = {
-  strings: ["Soy Desarrollador Front-end", "Soy Desarrollador Móvil", "Soy un apasionado de la tecnología"],
+  strings: ["Soy Ingeniero Informático...", "Soy Desarrollador...", "Soy un apasionado de la tecnología"],
   typeSpeed: 80,
   backSpeed: 50,
   backDelay: 1500,
